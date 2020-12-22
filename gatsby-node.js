5b681bdfc55f13d323b43819306ae6a627e71402
Jane Tracy Muthoni <muchirijane@outlook.com>
2020-12-22 21:35:18 +0300

    Generating dynamic pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -14,3 +14,40 @@ exports.onCreateNode = ({ node, actions }) => {
 
   }
 }
+
+exports.createPages = async ({ graphql, actions }) => {
+  const {createPage} = actions
+
+  const blogTempalte = path.resolve(`src/templates/blog.js`);
+  const res = await graphql(`
+    query{
+      allMarkdownRemark{
+        edges{
+          node{
+            fields{
+              slug
+            }
+            
+          }
+        }
+      }
+      
+    }
+  `)
+  
+
+  res.data.allMarkdownRemark.edges.forEach((edge) =>{
+    createPage({
+      component: blogTempalte,
+      path: `/blog/${edge.node.fields.slug}`,
+      context: {
+        slug: edge.node.fields.slug
+      }
+    })
+  })
+
+  console.log(res.status, res)
+  //1. Get path to hljs-template
+  //2. Get markdown data
+  //3. Create new changes
+}
