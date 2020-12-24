23f6c5eb453afb8ad29e91f52f8c4dbcee6e1875
Jane Tracy Muthoni <muchirijane@outlook.com>
2020-12-24 23:35:11 +0300

    adding images to md files

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,9 @@ module.exports = {
         implementation: require('sass')
       },
     },
+    `gatsby-plugin-sharp`,
+     `gatsby-transformer-sharp`,
+     `gatsby-transformer-remark`,
     {
       resolve: `gatsby-source-filesystem`,
       options:{
@@ -26,8 +29,73 @@ module.exports = {
       }
     },
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/posts`,
+      },
+    },
+    {
       resolve: `gatsby-transformer-remark`,
-      
-    }
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 750,
+            },
+          },
+        ],
+      },
+    },
   ],
 }
+
+// module.exports = {
+//   /* Your site config here */
+//   siteMetadata:{
+//     title: 'Jane Tracy',
+//     description: 'I\'m a 24 year old self-taught Front-End Developer',
+//     author: 'Jane Tracy'
+//   },
+//   plugins: [
+//     {
+//       resolve: `gatsby-plugin-sass`,
+//         options: {
+//         implementation: require('sass')
+//       },
+//     },
+//     {
+//       resolve: `gatsby-source-filesystem`,
+//       options:{
+//         name: 'src',
+//         path: `${__dirname}/src/`
+//       }
+//     },
+//     {
+//       resolve: `gatsby-source-filesystem`,
+//       options:{
+//         name: 'src',
+//         path: `${__dirname}/src/posts`
+//       }
+//     },
+//     `gatsby-plugin-sharp`,
+//     `gatsby-transformer-sharp`,
+//     `gatsby-transformer-remark`,
+//     {
+//       resolve: `gatsby-transformer-remark`,
+//       options:{
+//         plugins: [
+//           'gatsby-remark-relative-images-v2',
+//           {
+//             resolve : 'gatsby-remark-images',
+//             options: {
+//               maxWidth: 750,
+//               linkImagesToOriginal: false
+//             }
+//           }
+//         ]
+//       }
+      
+//     }
+//   ],
+// }
--- src/posts/react.md
+++ src/posts/react.md
@@ -4,3 +4,5 @@ date: "2020-12-18"
 ---
 
 In this post you will learn React.
+
+![React image](./react-resources.png)
