bda70d3fd2a0db4ce970d8f64084cbc722d213e1
Jane Tracy Muthoni <muchirijane@outlook.com>
2020-12-26 22:57:13 +0300

    initializing contentful cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,19 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
 
 module.exports = {
-  /* Your site config here */
+  
   siteMetadata:{
     title: 'Jane Tracy',
     description: 'I\'m a 24 year old self-taught Front-End Developer',
     author: 'Jane Tracy'
   },
   plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
     {
       resolve: `gatsby-plugin-sass`,
         options: {
